10d8d49282ce3db26d2537fc81f154e64ad6ce70
idenkov <ivan.denkov87@gmail.com>
2017-06-01 21:51:11 +0300

    Bash script for dekstop software

---
+++ ubuntu-desktop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#Script to install for Ubutnu Unity desktop to install some of the software I use
+#Should be run as root
+
+#Add additional repositories
+add-apt-repository -y ppa:nextcloud-devs/client
+add-apt-repository -y ppa:gencfsm
+
+#Update
+apt update
+
+#Remove unsused software
+apt purge -y rhythmbox thunderbird
+
+apt upgrade -y
+
+apt install -y software-center chromium-browser geany filezilla vlc gparted clementine compizconfig-settings-manager \
+hexchat virt-manager gimp pcmanfm gnome-encfs-manager nextcloud-client psensor vim htops transmission-gtk
+
+apt -y autoremove
+apt -y autoclean
