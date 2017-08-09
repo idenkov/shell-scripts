30c59bf1085fca693e174597bf8dbf99d809442f
idenkov <ivan.denkov87@gmail.com>
2017-08-10 00:08:01 +0300

    gzip compression

--- backup.sh
+++ backup.sh
@@ -5,6 +5,7 @@
 DATE=$(date +"%Y-%m-%d")
 MYSQLDUMP="$(which mysqldump)"
 TAR="$(which tar)"
+GZIP="$(which gzip)"
 PASS="MySQLPSWD"
 DIR="/home/backups"
 DBS=`mysql -u root -h localhost -p$PASS -e"show databases"`
@@ -14,18 +15,21 @@ FREEHD=`df -h`
 #This will remove files older than 35 days
 find $DIR/* -mtime +35 -exec rm -rf {} \;
 
-$TAR zcvf $DIR/Backup-$DATE.tar.gz /var/www
+$TAR cvf $DIR/Backup-$DATE.tar /var/www
 for DATABASE in $DBS
 do
 if [ $DATABASE != "Database" ]; then
 FILENAME=$DATABASE
-$MYSQLDUMP -u root -h localhost -p$PASS $DATABASE > $DIR/$FILENAME.sql
-$TAR rvf $DIR/Backup-$DATE.tar.gz $DIR/$FILENAME.sql
+$MYSQLDUMP -u root -h localhost -p$PASS $DATABASE | $GZIP > $DIR/$FILENAME.sql.gz
+$TAR vf $DIR/Backup-$DATE.tar $DIR/$FILENAME.sql.gz
 fi
 done
 
 #Remove dumped .sql in the folder
-rm -rf $DIR/*.sql
+rm -rf $DIR/*.sql.gz
+
+#gzip archives
+$GZIP $DIR/*.tar
 
 sleep 40
 
