e36ea2a5c736058cf14e660316235f2c5b65f2ea
Ivan Denkov <ivan.denkov87@gmail.com>
2019-06-27 01:17:11 +0300

    Update pma-install.sh

--- pma-install.sh
+++ pma-install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #This script will install phpmyadmin in each sub directory where wp-config.php file is found 
-#Author: Ivan Denkov
 
 #Edit this to change the directory where wordpress sites are.
-ROOT_DIR=/var/www
+ROOT_DIR=/srv/users
 
 RED='\033[0;31m'
 NC='\033[0m' # No Color
@@ -24,16 +23,21 @@ do
    HASH=$(md5sum $DIR/wp-config.php | cut -c1-12)
    HASHED_DIR=$DIR/pma-$HASH
 
+   if [ ! -d "$HASHED_DIR" ]; then
+     mkdir "$HASHED_DIR"
+   fi
+
+   tar xf pma.tar.gz --strip-components=1 -C $HASHED_DIR
+   
    printf "\n"
+
+   USER=$(stat -c '%U' $DIR/wp-config.php)
+   GROUP=$(stat -c '%G' $DIR/wp-config.php)
+   sudo chown -R $USER:$GROUP $HASHED_DIR
 
    echo -e "DB name: ${RED} `cat $DIR/wp-config.php | grep DB_NAME | cut -d \' -f 4` ${NC}"
    echo -e "DB user: ${RED} `cat $DIR/wp-config.php | grep DB_USER | cut -d \' -f 4` ${NC}"
    echo -e "DB password: ${RED} `cat $DIR/wp-config.php | grep DB_PASSWORD | cut -d \' -f 4` ${NC}"
    echo "Installed phpmyadmin in directory: `echo -e ${RED}$HASHED_DIR${NC}` "
 
-   if [ ! -d "$HASHED_DIR" ]; then
-     mkdir "$HASHED_DIR"
-   fi  
- 
-   tar xf pma.tar.gz --strip-components=2 -C $HASHED_DIR
-done 
+done	
